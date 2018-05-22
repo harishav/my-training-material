8ac198e8e867dabe9e56c57c6d0bc08f63e576c5
harishav <Harish.akuluru@outlook.com>
2018-05-22 10:05:52 +0530

    Added 4th tutorial

---
+++ Threads/src/udemyCourseSamples/com/harish/caveOfProgramming/_4_MultipleLocks_UsageOfSyncBlocks.java
@@ -0,0 +1,15 @@
+package com.harish.caveOfProgramming;
+
+/**
+ * Usage of Multiple locks and Synchronized blocks
+ * 
+ * @author harishav
+ *
+ */
+public class _4_MultipleLocks_UsageOfSyncBlocks {
+
+	public static void main(String[] args) {
+		new _4_Worker().main();
+	}
+
+}
