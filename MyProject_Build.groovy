9e3ccc75037f79728232e9af996e2b6f4446701f
harishav <Harish.akuluru@outlook.com>
2018-11-22 15:15:21 +0530

    Updated

--- MyProject_Build.groovy
+++ MyProject_Build.groovy
@@ -8,7 +8,7 @@ job("MyProject_Build") {
     scm {
         git {
             remote {
-                url gitUrl.
+                url gitUrl
                 branch "origin/master"
             }
             extensions {
