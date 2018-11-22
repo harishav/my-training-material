a74fd1d93aa67dddb9868d3cc493920c0c4b15a5
harishav <Harish.akuluru@outlook.com>
2018-11-22 15:04:41 +0530

    Create MyProject-Build groovy file
    Added this file to create a job using the SeedJob in Jenkins

---
+++ MyProject-Build.groovy
@@ -0,0 +1,23 @@
+def gitUrl = "https://github.com/example/project.git"
+
+job("MyProject-Build") {
+    description "Builds MyProject from master branch."
+    parameters {
+        stringParam('COMMIT', 'HEAD', 'Commit to build')
+    }
+    scm {
+        git {
+            remote {
+                url gitUrl.
+                branch "origin/master"
+            }
+            extensions {
+                wipeOutWorkspace()
+                localBranch master
+            }
+        }
+    }
+    steps {
+        shell "Look: I'm building master!"
+    }
+}
